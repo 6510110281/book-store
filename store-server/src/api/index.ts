06312721bd047e77c0411048e28c09d0ebd732ea
ปุญญพัฒน์ แก้วขาว <aimaster0010@gmail.com>
2022-12-16 14:55:22 +0700

    เพิ่มการเรียกใช้ api/book

--- store-server/src/api/index.ts
+++ store-server/src/api/index.ts
@@ -1,5 +1,6 @@
 import Router from 'koa-router'
 import category from './category'
+import book from './book'
 
 const apiRouter = new Router()
 
@@ -8,5 +9,6 @@ apiRouter.get('/api/greet', async (ctx, next) => {
 })
   
 apiRouter.use('/api/category', category.routes())
+apiRouter.use('/api/book', book.routes())
 
 export default apiRouter
